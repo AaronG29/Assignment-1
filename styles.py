d810b0af64626676e87ccd43a9bbfdfd316bad63
AaronG29 <aar010n@terpmail.umd.edu>
2026-03-08 19:17:42 -0400

    Added styles

---
+++ styles.py
@@ -0,0 +1,104 @@
+#Starting styles for the site
+
+
+body{
+    font-family: Arial;
+    margin:0;
+}
+
+
+
+
+header{
+    background:#222;
+    color:white;
+    text-align:center;
+    padding:15px;
+}
+
+
+
+
+nav ul{
+    list-style:none;
+    margin:0;
+    padding:10px;
+    background:#444;
+}
+
+
+
+
+nav li{
+    display:inline;
+    margin-right:15px;
+}
+
+
+
+
+
+nav a{
+    color:white;
+    text-decoration:none;
+}
+
+
+
+
+nav li:hover{
+    background:yellow;
+}
+
+
+
+footer{
+    background:#222;
+    color:white;
+    text-align:center;
+    padding:10px;
+    margin-top:20px;
+}
+
+
+
+.main{
+    width:60%;
+    float:left;
+    border:2px solid black;
+    padding:10px;
+}
+
+
+
+.sidebar{
+    width:20%;
+    float:left;
+    border:2px solid black;
+    padding:10px;
+    margin-left:10px;
+}
+
+
+
+
+.section{
+    border:1px solid gray;
+    margin-bottom:10px;
+    padding:5px;
+}
+
+
+
+.box{
+    width:40%;
+    border:2px solid black;
+    padding:10px;
+    display:inline-block;
+}
+
+
+
+button:hover{
+    background:lightgreen;
+}
